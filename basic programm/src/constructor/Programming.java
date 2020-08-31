17313eefb75693d4de2a3eb5b3e2b451e59ae0c4
shubham deokar <sdeokar13599@gamil.comgit config --global user.name  shubham>
2020-08-31 10:25:06 +0530

    constructor

---
+++ basic programm/src/constructor/Programming.java
@@ -0,0 +1,20 @@
+package constructor;
+
+public class Programming {
+	String name;
+	
+	public Programming()
+	{
+		
+		System.out.println("I want to learn language");
+	}
+
+	
+	public Programming(String language)
+	{
+		
+		name =language;
+		System.out.println(" I want to learn "+name);
+		
+	}
+}
---
+++ basic programm/src/constructor/ProgrammingMain.java
@@ -0,0 +1,15 @@
+package constructor;
+
+import java.util.Scanner;
+
+public class ProgrammingMain {
+Scanner scr = new Scanner(System.in);
+	public static void main(String[] args) {
+	new Programming();
+	new Programming("java");
+	
+	
+
+	}
+
+}
